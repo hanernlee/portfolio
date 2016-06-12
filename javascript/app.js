a363d4e2b9903751ba4c09f0764ba0f3f7d9144e
Christopher Lee <hanernlee@gmail.com>
2016-06-12 18:15:22 +1000

    edit hover

--- javascript/app.js
+++ javascript/app.js
@@ -3,9 +3,7 @@ $(document).ready(function () {
   $('#about').on('click', function() {
     if ($('#about-section').is(':hidden')) {
       $('#about-section').css("display","block");
-      $('#about').css({'opacity':'0','pointer-events':'none'});
-      $('#work').css({'opacity':'0','pointer-events':'none'});
-      $('#contact').css({'opacity':'0','pointer-events':'none'});
+      $('#navigation').css('display','none');
       $('#name-container').hide();
     }
   });
@@ -13,9 +11,7 @@ $(document).ready(function () {
   $('#work').on('click', function() {
     if ($('#work-section').is(':hidden')) {
       $('#work-section').css("display","block");
-      $('#about').css({'opacity':'0','pointer-events':'none'});
-      $('#work').css({'opacity':'0','pointer-events':'none'});
-      $('#contact').css({'opacity':'0','pointer-events':'none'});
+      $('#navigation').css('display','none');
       $('#name-container').hide();
     }
   });
@@ -23,9 +19,7 @@ $(document).ready(function () {
   $('#contact').on('click', function() {
     if ($('#contact-section').is(':hidden')) {
       $('#contact-section').css("display","block");
-      $('#about').css({'opacity':'0','pointer-events':'none'});
-      $('#work').css({'opacity':'0','pointer-events':'none'});
-      $('#contact').css({'opacity':'0','pointer-events':'none'});
+      $('#navigation').css('display','none');
       $('#name-container').hide();
     }
   });
@@ -35,10 +29,7 @@ $(document).ready(function () {
     $('#about-section').css("display","none");
     $('#work-section').css("display","none");
     $('#contact-section').css("display","none");
-
-    $('#work').css({'opacity':'1','pointer-events':'auto'});
-    $('#contact').css({'opacity':'1','pointer-events':'auto'});
-    $('#about').css({'opacity':'1','pointer-events':'auto'});
+    $('#navigation').css('display','block');
     $('#name-container').show();
   });
 
