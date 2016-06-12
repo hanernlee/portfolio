7a003a08b735f4835fa1c6eb5a8821c520a32950
Christopher Lee <hanernlee@gmail.com>
2016-06-12 12:53:41 +1000

    added work section

--- javascript/app.js
+++ javascript/app.js
@@ -2,51 +2,44 @@ $(document).ready(function () {
 
   $('#about').on('click', function() {
     if ($('#about-section').is(':hidden')) {
-      $(this).find('i').removeClass('fa-user').addClass('fa-close');
-      $('#about-section').css("display","flex");
+      $('#about-section').css("display","block");
+      $('#about').css({'opacity':'0','pointer-events':'none'});
       $('#work').css({'opacity':'0','pointer-events':'none'});
       $('#contact').css({'opacity':'0','pointer-events':'none'});
       $('#name-container').hide();
-    } else {
-      $(this).find('i').removeClass('fa-close').addClass('fa-user');
-      $('#about-section').css("display","none");
-      $('#work').css({'opacity':'1','pointer-events':'auto'});
-      $('#contact').css({'opacity':'1','pointer-events':'auto'});
-      $('#name-container').show();
     }
   });
 
-
   $('#work').on('click', function() {
     if ($('#work-section').is(':hidden')) {
-      $(this).find('i').removeClass('fa-briefcase').addClass('fa-close');
-      $('#work-section').css("display","flex");
+      $('#work-section').css("display","block");
       $('#about').css({'opacity':'0','pointer-events':'none'});
+      $('#work').css({'opacity':'0','pointer-events':'none'});
       $('#contact').css({'opacity':'0','pointer-events':'none'});
       $('#name-container').hide();
-    } else {
-      $(this).find('i').removeClass('fa-close').addClass('fa-briefcase');
-      $('#work-section').css("display","none");
-      $('#about').css({'opacity':'1','pointer-events':'auto'});
-      $('#contact').css({'opacity':'1','pointer-events':'auto'});
-      $('#name-container').show();
     }
   });
 
   $('#contact').on('click', function() {
     if ($('#contact-section').is(':hidden')) {
-      $(this).find('i').removeClass('fa-phone').addClass('fa-close');
-      $('#contact-section').css("display","flex");
+      $('#contact-section').css("display","block");
       $('#about').css({'opacity':'0','pointer-events':'none'});
       $('#work').css({'opacity':'0','pointer-events':'none'});
+      $('#contact').css({'opacity':'0','pointer-events':'none'});
       $('#name-container').hide();
-    } else {
-      $(this).find('i').removeClass('fa-close').addClass('fa-phone');
-      $('#contact-section').css("display","none");
-      $('#about').css({'opacity':'1','pointer-events':'auto'});
-      $('#work').css({'opacity':'1','pointer-events':'auto'});
-      $('#name-container').show();
     }
+  });
+
+
+  $('.close-btn').on('click', function() {
+    $('#about-section').css("display","none");
+    $('#work-section').css("display","none");
+    $('#contact-section').css("display","none");
+
+    $('#work').css({'opacity':'1','pointer-events':'auto'});
+    $('#contact').css({'opacity':'1','pointer-events':'auto'});
+    $('#about').css({'opacity':'1','pointer-events':'auto'});
+    $('#name-container').show();
   });
 
 
